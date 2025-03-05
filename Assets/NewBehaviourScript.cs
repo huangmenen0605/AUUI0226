141dfe0cafe3a3dfe70b1363bd518a8ec144b5e0
huangmenen0605 <huangmenen0605@gmail.com>
2025-03-05 11:13:18 +0800

    03051113

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -1,18 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    
+    private void Awake()
     {
+        Debug.Log(message:"你也開始Awake");
+    }
+    
+    private void Start()
+    {
+        Debug.Log(message:"我在開始Start");
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        Debug.Log(message:"她也開始OnEnable");
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log(message:"他也開始OnDisable");
     }
 }
