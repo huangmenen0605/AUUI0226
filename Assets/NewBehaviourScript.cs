1e25eb3c17c91aba652914350e69e9e899d94450
huangmenen0605 <huangmenen0605@gmail.com>
2025-03-12 11:51:00 +0800

    完成

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -3,17 +3,20 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField]
+    private GameObject cubeee;
 
-   
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log(message:"OnTriggerEnter");
+        //GameObject cubeee = GameObject.Find("Cubeee");
+        cubeee.SetActive(true);
     }
 
     private void OnCollisionEnter(Collision other)
     {
         Debug.Log(message:"OnCollisionEnter");
-        other.gameObject.SetActive(false);
+        //other.gameObject.SetActive(false);
     }
 }
