cf511c41f6b69f33dc9fe5d5453c433cb0b8368f
huangmenen0605 <huangmenen0605@gmail.com>
2025-04-02 12:03:34 +0800

    4/2練習

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,7 +1,26 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Character : MonoBehaviour
 {
+  public void AttackAllTriggeredEnemies()
+  {
+    foreach (var enemy in enemies)
+    {
+      enemy.TakeDamage(atk);
+    }
+  }
+
+  private List<Enemy> enemies = new List<Enemy>();
+
+  public void OnTriggerEnter2D(Collider2D collider2D)
+  {
+    var enemy = collider2D.GetComponent<Enemy>();
+    enemies.Add(enemy);
+
+  }
+
   private int atk;
   public void Attack(Enemy enemy)
   {
--- Assets/Tests/CharacterTest.cs
+++ Assets/Tests/CharacterTest.cs
@@ -47,5 +47,29 @@ public class CharacterTest
         var hp = enemy.GetHp();
         Assert.AreEqual(0,hp);
     }
+
+    [Test(Description = "角色攻擊偵測區域內的所有敵人")]
+    public void Character_Attack_All_Triggered_Enemies()
+    {
+        //arrange
+        //玩家角色元件
+        var character = new GameObject().AddComponent<Character>();
+        character.SetAtk(10);
+        //敵人元件
+        var enemy1 = new GameObject().AddComponent<Enemy>();
+        var enemy1Collider = enemy1.gameObject.AddComponent<BoxCollider2D>();
+        enemy1.SetHp(100);
+        var enemy2 = new GameObject().AddComponent<Enemy>();
+        var enemy2Collider = enemy2.gameObject.AddComponent<BoxCollider2D>();
+        enemy2.SetHp(100);
+        character.OnTriggerEnter2D(enemy1Collider);
+        character.OnTriggerEnter2D(enemy2Collider);
+
+        //act
+        character.AttackAllTriggeredEnemies();
+        //assert
+        Assert.AreEqual(90,  enemy1.GetHp());
+        Assert.AreEqual(90 , enemy2.GetHp());
+    }
     
 }
